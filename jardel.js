74a25e6759cdfcf21647bbb3607bb224287a8f14
Jardel <jardeldiasb1@outlook.com>
2024-05-13 10:17:35 -0300

    primeiro commit

---
+++ jardel.js
@@ -0,0 +1,14 @@
+function myFunction() {
+     var x = document.getElementById("myLinks");
+     if (x.style.display === "block") {
+         x.style.display = "none";
+     } else {
+         x.style.display = "block";
+     }
+}
+ 
+
+     var btn = document.querySelector("#back-to-top");
+     btn.addEventListener("click", function() {
+         window.scrollTo(0, 0);
+     });
